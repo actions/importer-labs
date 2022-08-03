2a3a675a3a8ff56b72d9b856dd2f9fc7bda35555
Begona Guereca <begonaguereca@github.com>
2022-08-03 00:01:44 +0000

    building dockerfile to automate

---
+++ security.groovy
@@ -0,0 +1,20 @@
+#!groovy
+ 
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+ 
+def instance = Jenkins.getInstance()
+ 
+def user = new File("/run/secrets/jenkins-user").text.trim()
+def pass = new File("/run/secrets/jenkins-pass").text.trim()
+ 
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(user, pass)
+instance.setSecurityRealm(hudsonRealm)
+ 
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
+ 
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
