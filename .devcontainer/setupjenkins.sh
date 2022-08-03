58e6dd7d690f3e0a2595cd132e42e232a417ec98
Begona Guereca <begonaguereca@github.com>
2022-08-03 23:20:11 +0000

    Create shell script to execute docker commands

---
+++ .devcontainer/setupjenkins.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+container_name="jenkins"
+username="admin"
+password="password"
+
+echo "Building Jenkins instance!"
+
+# Build jenkins image from docker compose file 
+docker build -t jenkins:$container_name $(pwd)/jenkins/jenkins_setup
+
+# wait until docker image is ready
+sleep 2
+
+# Build container
+docker run --name jenkins --rm -p 8080:8080 --env JENKINS_ADMIN_ID=$username --env JENKINS_ADMIN_PASSWORD=$password jenkins:$container_name
+
