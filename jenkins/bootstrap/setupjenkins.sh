517714bcce7d44d58bf83aec889a51fa2fc7c61b
Begona Guereca <begonaguereca@github.com>
2022-08-08 16:34:16 -0700

    change and commit file permissions for this file to be executable

--- jenkins/bootstrap/setupjenkins.sh
+++ jenkins/bootstrap/setupjenkins.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 container_name="valet"
 username="admin"
@@ -8,9 +8,6 @@ echo "Building Jenkins instance!"
 
 # Build jenkins image from docker compose file 
 docker build -t jenkins:$container_name .
-
-# wait until docker image is ready
-sleep 2
 
 # Build container
 docker run -d --name jenkins -p 8080:8080 --env JENKINS_ADMIN_ID=$username --env JENKINS_ADMIN_PASSWORD=$password jenkins:$container_name
