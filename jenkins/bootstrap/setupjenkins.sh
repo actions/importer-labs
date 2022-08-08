7a39fce1ffff1c1b2822255b4f08ed03ecfecff0
Begona Guereca <begonaguereca@github.com>
2022-08-08 16:06:57 -0700

    Update jenkins/bootstrap/setupjenkins.sh
    Co-authored-by: Ethan Dennis <ethanis@github.com>

--- jenkins/bootstrap/setupjenkins.sh
+++ jenkins/bootstrap/setupjenkins.sh
@@ -13,4 +13,4 @@ docker build -t jenkins:$container_name .
 sleep 2
 
 # Build container
-docker run --name jenkins -p 8080:8080 --env JENKINS_ADMIN_ID=$username --env JENKINS_ADMIN_PASSWORD=$password jenkins:$container_name
+docker run -d --name jenkins -p 8080:8080 --env JENKINS_ADMIN_ID=$username --env JENKINS_ADMIN_PASSWORD=$password jenkins:$container_name
