ff19794701c8ac121bfa9d2f45664f3726612f51
Begona Guereca <begonaguereca@github.com>
2022-08-09 10:57:03 -0700

    Add logic to help control flow of setup script better

--- jenkins/bootstrap/setupjenkins.sh
+++ jenkins/bootstrap/setupjenkins.sh
@@ -6,8 +6,24 @@ password="password"
 
 echo "Building Jenkins instance!"
 
-# Build jenkins image from docker compose file 
-docker build -t jenkins:$container_name .
+if [ "$(docker ps -a | grep jenkins:$container_name)" ]; then
+  echo -e "Jenkins is running"
+  docker container start jenkins
+else
+  echo -e "\nStarting a new Jenkins container"
+  # Build jenkins image from docker compose file 
+  docker build -t jenkins:$container_name .
 
-# Build container
-docker run -d --name jenkins -p 8080:8080 --env JENKINS_ADMIN_ID=$username --env JENKINS_ADMIN_PASSWORD=$password jenkins:$container_name
+  # Build container
+  docker run -d --name jenkins -p 8080:8080 --env JENKINS_ADMIN_ID=$username --env JENKINS_ADMIN_PASSWORD=$password jenkins:$container_name
+fi
+
+echo -e "\nWaiting for Jenkins to start..."
+while ! curl -s http://localhost:8080/ > /dev/null; do
+  echo -e "."
+  sleep 5
+done
+
+echo -e '\nJenkins is up and running!'
+echo -e "\nUsername: admin"
+echo -e "\bPassword: password"
